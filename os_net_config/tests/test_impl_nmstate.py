b26b9734347e45c0b145773b6109a9f19e54b0ec
Karthik Sundaravel <ksundara@redhat.com>
2024-09-14 03:39:54 +0530

    Add stubs for show_running_config() to fix linter jobs

--- os_net_config/tests/test_impl_nmstate.py
+++ os_net_config/tests/test_impl_nmstate.py
@@ -192,6 +192,16 @@ _V6_NMCFG_MULTIPLE = _V6_NMCFG + """    - ip: 2001:abc:b::1
 class TestNmstateNetConfig(base.TestCase):
     def setUp(self):
         super(TestNmstateNetConfig, self).setUp()
+
+        def show_running_info_stub():
+            running_info_path = os.path.join(
+                os.path.dirname(__file__),
+                'environment/netinfo_running_info_1.yaml')
+            running_info = self.get_running_info(running_info_path)
+            return running_info
+        self.stub_out('libnmstate.netinfo.show_running_config',
+                      show_running_info_stub)
+
         self.temp_route_table_file = tempfile.NamedTemporaryFile()
         self.provider = impl_nmstate.NmstateNetConfig()
 
@@ -211,6 +221,11 @@ class TestNmstateNetConfig(base.TestCase):
             'os_net_config.impl_nmstate.route_table_config_path',
             test_route_table_path)
         utils.write_config(self.temp_route_table_file.name, _RT_CUSTOM)
+
+    def get_running_info(self, yaml_file):
+        with open(yaml_file) as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+            return data
 
     def get_interface_config(self, name='em1'):
         return self.provider.interface_data[name]
@@ -2196,6 +2211,16 @@ class TestNmstateNetConfigApply(base.TestCase):
             return None
         self.stub_out(
             'libnmstate.netapplier.apply', test_iface_state)
+
+        def show_running_info_stub():
+            running_info_path = os.path.join(
+                os.path.dirname(__file__),
+                'environment/netinfo_running_info_1.yaml')
+            running_info = self.get_running_info(running_info_path)
+            return running_info
+        self.stub_out('libnmstate.netinfo.show_running_config',
+                      show_running_info_stub)
+
         self.provider = impl_nmstate.NmstateNetConfig()
 
         def get_totalvfs_stub(iface_name):
@@ -2218,16 +2243,6 @@ class TestNmstateNetConfigApply(base.TestCase):
         super(TestNmstateNetConfigApply, self).tearDown()
 
     def test_base_interface(self):
-
-        def show_running_info_stub():
-            running_info_path = os.path.join(
-                os.path.dirname(__file__),
-                'environment/netinfo_running_info_1.yaml')
-            running_info = self.get_running_info(running_info_path)
-            return running_info
-        self.stub_out('libnmstate.netinfo.show_running_config',
-                      show_running_info_stub)
-
         self.add_object(_BASE_IFACE_CFG)
         updated_files = self.provider.apply()
         self.assertEqual(yaml.load(_BASE_IFACE_CFG_APPLIED,
