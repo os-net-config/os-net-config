karthiksundaravel/os-net-config_nmstate

Adoption from ifcfg to nmstate provider
Add support for reconfiguring the network interfaces from ifcfg
provider to nmstate in existing deployments. The steps are
1. Run ifdown on all intrerfaces
2. Move ifcfg files from /etc/sysconfig/network-scripts/ to
/var/lib/os-net-config/ifcfg-purge/
3. Run ```nmcli con reload```
4. Mark the device as absent in nmstate provider
5. Configure the device with nmstate provider
6. Wipe the residue files from ifcfg provider

os_net_config/__init__.py
@@ -14,6 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import json
 import logging
 import os
 
@@ -42,6 +43,65 @@ class NetConfig(object):
         self.log_prefix = "NOOP: " if noop else ""
         self.root_dir = root_dir
         self.errors = []
+
+    def del_object(self, obj):
+        if isinstance(obj, objects.RouteTable):
+            self.del_route_table(obj)
+        elif isinstance(obj, objects.Interface):
+            self.del_interface(obj)
+        elif isinstance(obj, objects.Vlan):
+            self.del_vlan(obj)
+        elif isinstance(obj, objects.IvsInterface):
+            self.del_ivs_interface(obj)
+        elif isinstance(obj, objects.NfvswitchInternal):
+            self.del_nfvswitch_internal(obj)
+        elif isinstance(obj, objects.OvsBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_bridge(obj)
+        elif isinstance(obj, objects.OvsUserBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_ovs_user_bridge(obj)
+        elif isinstance(obj, objects.IvsBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_ivs_bridge(obj)
+        elif isinstance(obj, objects.NfvswitchBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_nfvswitch_bridge(obj)
+        elif isinstance(obj, objects.OvsBond):
+            self.del_bond(obj)
+            for member in obj.members:
+                self.del_object(member)
+        elif isinstance(obj, objects.LinuxBond):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_linux_bond(obj)
+        elif isinstance(obj, objects.IbInterface):
+            self.del_ib_interface(obj)
+        elif isinstance(obj, objects.IbChildInterface):
+            self.del_ib_child_interface(obj)
+        elif isinstance(obj, objects.OvsDpdkPort):
+            self.del_ovs_dpdk_port(obj)
+        elif isinstance(obj, objects.OvsDpdkBond):
+            self.del_ovs_dpdk_bond(obj)
+        elif isinstance(obj, objects.SriovPF):
+            self.del_sriov_pf(obj)
+        elif isinstance(obj, objects.SriovVF):
+            self.del_sriov_vf(obj)
+        else:
+            obj_type = json.get("type")
+            logger.error(f'del_object not handled for {obj_type}')
+
+    def enable_migration():
+        logger.info('Migration is not supported for this provider')
+        return
+
+    def clean_migration(self):
+        logger.info('clean migration is not supported for this provider')
+        return
 
     def add_object(self, obj):
         """Convenience method to add any type of object to the network config.
@@ -128,12 +188,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_route_table is not implemented.")
 
+    def del_route_table(self, route_table):
+        """Delete  a route table object to the net config object.
+
+        :param route_table: The RouteTable object to del.
+        """
+        raise NotImplementedError("del_route_table is not implemented.")
+
     def add_interface(self, interface):
         """Add an Interface object to the net config object.
 
         :param interface: The Interface object to add.
         """
         raise NotImplementedError("add_interface is not implemented.")
+
+    def del_interface(self, interface):
+        """del an Interface object to the net config object.
+
+        :param interface: The Interface object to add.
+        """
+        raise NotImplementedError("del_interface is not implemented.")
 
     def add_vlan(self, vlan):
         """Add a Vlan object to the net config object.
@@ -142,6 +216,13 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_vlan is not implemented.")
 
+    def del_vlan(self, vlan):
+        """del a Vlan object to the net config object.
+
+        :param vlan: The vlan object to del.
+        """
+        raise NotImplementedError("del_vlan is not implemented.")
+
     def add_bridge(self, bridge):
         """Add an OvsBridge object to the net config object.
 
@@ -149,12 +230,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_bridge is not implemented.")
 
+    def del_bridge(self, bridge):
+        """Del an OvsBridge object to the net config object.
+
+        :param bridge: The OvsBridge object to del.
+        """
+        raise NotImplementedError("del_bridge is not implemented.")
+
     def add_ovs_user_bridge(self, bridge):
         """Add an OvsUserBridge object to the net config object.
 
         :param bridge: The OvsUserBridge object to add.
         """
         raise NotImplementedError("add_ovs_user_bridge is not implemented.")
+
+    def del_ovs_user_bridge(self, bridge):
+        """Del an OvsUserBridge object to the net config object.
+
+        :param bridge: The OvsUserBridge object to del.
+        """
+        raise NotImplementedError("del_ovs_user_bridge is not implemented.")
 
     def add_linux_bridge(self, bridge):
         """Add a LinuxBridge object to the net config object.
@@ -170,6 +265,13 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_ivs_bridge is not implemented.")
 
+    def del_ivs_bridge(self, bridge):
+        """Del a IvsBridge object to the net config object.
+
+        :param bridge: The IvsBridge object to del.
+        """
+        raise NotImplementedError("del_ivs_bridge is not implemented.")
+
     def add_nfvswitch_bridge(self, bridge):
         """Add a NfvswitchBridge object to the net config object.
 
@@ -177,12 +279,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_nfvswitch_bridge is not implemented.")
 
+    def del_nfvswitch_bridge(self, bridge):
+        """Del a NfvswitchBridge object to the net config object.
+
+        :param bridge: The NfvswitchBridge object to del.
+        """
+        raise NotImplementedError("del_nfvswitch_bridge is not implemented.")
+
     def add_bond(self, bond):
         """Add an OvsBond object to the net config object.
 
         :param bond: The OvsBond object to add.
         """
         raise NotImplementedError("add_bond is not implemented.")
+
+    def del_bond(self, bond):
+        """Del an OvsBond object to the net config object.
+
+        :param bond: The OvsBond object to del.
+        """
+        raise NotImplementedError("del_bond is not implemented.")
 
     def add_linux_bond(self, bond):
         """Add a LinuxBond object to the net config object.
@@ -212,12 +328,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_ovs_patch_port is not implemented.")
 
+    def del_ovs_patch_port(self, ovs_patch_port):
+        """Del a OvsPatchPort object to the net config object.
+
+        :param ovs_patch_port: The OvsPatchPort object to del.
+        """
+        raise NotImplementedError("del_ovs_patch_port is not implemented.")
+
     def add_ib_interface(self, ib_interface):
         """Add an InfiniBand Interface object to the net config object.
 
         :param interface: The InfiniBand Interface object to add.
         """
         raise NotImplementedError("add_ib_interface is not implemented.")
+
+    def del_ib_interface(self, ib_interface):
+        """Del an InfiniBand Interface object to the net config object.
+
+        :param interface: The InfiniBand Interface object to del.
+        """
+        raise NotImplementedError("del_ib_interface is not implemented.")
 
     def add_ib_child_interface(self, ib_child_interface):
         """Add an InfiniBand child interface object to the net config object.
@@ -234,12 +364,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_ovs_dpdk_port is not implemented.")
 
+    def del_ovs_dpdk_port(self, ovs_dpdk_port):
+        """Del a OvsDpdkPort object to the net config object.
+
+        :param ovs_dpdk_port: The OvsDpdkPort object to del.
+        """
+        raise NotImplementedError("del_ovs_dpdk_port is not implemented.")
+
     def add_ovs_dpdk_bond(self, ovs_dpdk_bond):
         """Add a OvsDpdkBond object to the net config object.
 
         :param ovs_dpdk_bond: The OvsDpdkBond object to add.
         """
         raise NotImplementedError("add_ovs_dpdk_bond is not implemented.")
+
+    def del_ovs_dpdk_bond(self, ovs_dpdk_bond):
+        """Del a OvsDpdkBond object to the net config object.
+
+        :param ovs_dpdk_bond: The OvsDpdkBond object to del.
+        """
+        raise NotImplementedError("del_ovs_dpdk_bond is not implemented.")
 
     def add_sriov_pf(self, sriov_pf):
         """Add a SriovPF object to the net config object.
@@ -248,12 +392,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_sriov_pf is not implemented.")
 
+    def del_sriov_pf(self, sriov_pf):
+        """Del a SriovPF object to the net config object.
+
+        :param sriov_pf: The SriovPF object to del.
+        """
+        raise NotImplementedError("del_sriov_pf is not implemented.")
+
     def add_sriov_vf(self, sriov_vf):
         """Add a SriovVF object to the net config object.
 
         :param sriov_vf: The SriovVF object to add.
         """
         raise NotImplementedError("add_sriov_vf is not implemented.")
+
+    def del_sriov_vf(self, sriov_vf):
+        """Del a SriovVF object to the net config object.
+
+        :param sriov_vf: The SriovVF object to del.
+        """
+        raise NotImplementedError("del_sriov_vf is not implemented.")
 
     def add_vpp_interface(self, vpp_interface):
         """Add a VppInterface object to the net config object.

os-net-config/os-net-config

(cherry picked from commit 47fa739d8476c207b7565b1a2c4e199afe49cc97)
Signed-off-by: Karthik Sundaravel <ksundara@redhat.com>

os_net_config/__init__.py
@@ -14,6 +14,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import json
 import logging
 import os
 
@@ -42,6 +43,65 @@ class NetConfig(object):
         self.log_prefix = "NOOP: " if noop else ""
         self.root_dir = root_dir
         self.errors = []
+
+    def del_object(self, obj):
+        if isinstance(obj, objects.RouteTable):
+            self.del_route_table(obj)
+        elif isinstance(obj, objects.Interface):
+            self.del_interface(obj)
+        elif isinstance(obj, objects.Vlan):
+            self.del_vlan(obj)
+        elif isinstance(obj, objects.IvsInterface):
+            self.del_ivs_interface(obj)
+        elif isinstance(obj, objects.NfvswitchInternal):
+            self.del_nfvswitch_internal(obj)
+        elif isinstance(obj, objects.OvsBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_bridge(obj)
+        elif isinstance(obj, objects.OvsUserBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_ovs_user_bridge(obj)
+        elif isinstance(obj, objects.IvsBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_ivs_bridge(obj)
+        elif isinstance(obj, objects.NfvswitchBridge):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_nfvswitch_bridge(obj)
+        elif isinstance(obj, objects.OvsBond):
+            self.del_bond(obj)
+            for member in obj.members:
+                self.del_object(member)
+        elif isinstance(obj, objects.LinuxBond):
+            for member in obj.members:
+                self.del_object(member)
+            self.del_linux_bond(obj)
+        elif isinstance(obj, objects.IbInterface):
+            self.del_ib_interface(obj)
+        elif isinstance(obj, objects.IbChildInterface):
+            self.del_ib_child_interface(obj)
+        elif isinstance(obj, objects.OvsDpdkPort):
+            self.del_ovs_dpdk_port(obj)
+        elif isinstance(obj, objects.OvsDpdkBond):
+            self.del_ovs_dpdk_bond(obj)
+        elif isinstance(obj, objects.SriovPF):
+            self.del_sriov_pf(obj)
+        elif isinstance(obj, objects.SriovVF):
+            self.del_sriov_vf(obj)
+        else:
+            obj_type = json.get("type")
+            logger.error(f'del_object not handled for {obj_type}')
+
+    def enable_migration():
+        logger.info('Migration is not supported for this provider')
+        return
+
+    def clean_migration(self):
+        logger.info('clean migration is not supported for this provider')
+        return
 
     def add_object(self, obj):
         """Convenience method to add any type of object to the network config.
@@ -128,12 +188,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_route_table is not implemented.")
 
+    def del_route_table(self, route_table):
+        """Delete a route table object to the net config object.
+
+        :param route_table: The RouteTable object to be deleted.
+        """
+        raise NotImplementedError("del_route_table is not implemented.")
+
     def add_interface(self, interface):
         """Add an Interface object to the net config object.
 
         :param interface: The Interface object to add.
         """
         raise NotImplementedError("add_interface is not implemented.")
+
+    def del_interface(self, interface):
+        """Delete the Interface object to the net config object.
+
+        :param interface: The Interface object to be deleted.
+        """
+        raise NotImplementedError("del_interface is not implemented.")
 
     def add_vlan(self, vlan):
         """Add a Vlan object to the net config object.
@@ -142,6 +216,13 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_vlan is not implemented.")
 
+    def del_vlan(self, vlan):
+        """Delete a Vlan object to the net config object.
+
+        :param vlan: The vlan object to be deleted.
+        """
+        raise NotImplementedError("del_vlan is not implemented.")
+
     def add_bridge(self, bridge):
         """Add an OvsBridge object to the net config object.
 
@@ -149,12 +230,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_bridge is not implemented.")
 
+    def del_bridge(self, bridge):
+        """Delete an OvsBridge object to the net config object.
+
+        :param bridge: The OvsBridge object to be deleted.
+        """
+        raise NotImplementedError("del_bridge is not implemented.")
+
     def add_ovs_user_bridge(self, bridge):
         """Add an OvsUserBridge object to the net config object.
 
         :param bridge: The OvsUserBridge object to add.
         """
         raise NotImplementedError("add_ovs_user_bridge is not implemented.")
+
+    def del_ovs_user_bridge(self, bridge):
+        """Delete an OvsUserBridge object to the net config object.
+
+        :param bridge: The OvsUserBridge object to be deleted.
+        """
+        raise NotImplementedError("del_ovs_user_bridge is not implemented.")
 
     def add_linux_bridge(self, bridge):
         """Add a LinuxBridge object to the net config object.
@@ -170,6 +265,13 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_ivs_bridge is not implemented.")
 
+    def del_ivs_bridge(self, bridge):
+        """Delete a IvsBridge object to the net config object.
+
+        :param bridge: The IvsBridge object to be deleted.
+        """
+        raise NotImplementedError("del_ivs_bridge is not implemented.")
+
     def add_nfvswitch_bridge(self, bridge):
         """Add a NfvswitchBridge object to the net config object.
 
@@ -177,12 +279,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_nfvswitch_bridge is not implemented.")
 
+    def del_nfvswitch_bridge(self, bridge):
+        """Delete a NfvswitchBridge object to the net config object.
+
+        :param bridge: The NfvswitchBridge object to be deleted.
+        """
+        raise NotImplementedError("del_nfvswitch_bridge is not implemented.")
+
     def add_bond(self, bond):
         """Add an OvsBond object to the net config object.
 
         :param bond: The OvsBond object to add.
         """
         raise NotImplementedError("add_bond is not implemented.")
+
+    def del_bond(self, bond):
+        """Delete an OvsBond object to the net config object.
+
+        :param bond: The OvsBond object to deleted.
+        """
+        raise NotImplementedError("del_bond is not implemented.")
 
     def add_linux_bond(self, bond):
         """Add a LinuxBond object to the net config object.
@@ -212,12 +328,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_ovs_patch_port is not implemented.")
 
+    def del_ovs_patch_port(self, ovs_patch_port):
+        """Delete a OvsPatchPort object to the net config object.
+
+        :param ovs_patch_port: The OvsPatchPort object to be deleted.
+        """
+        raise NotImplementedError("del_ovs_patch_port is not implemented.")
+
     def add_ib_interface(self, ib_interface):
         """Add an InfiniBand Interface object to the net config object.
 
         :param interface: The InfiniBand Interface object to add.
         """
         raise NotImplementedError("add_ib_interface is not implemented.")
+
+    def del_ib_interface(self, ib_interface):
+        """Delete an InfiniBand Interface object to the net config object.
+
+        :param interface: The InfiniBand Interface object to be deleted.
+        """
+        raise NotImplementedError("del_ib_interface is not implemented.")
 
     def add_ib_child_interface(self, ib_child_interface):
         """Add an InfiniBand child interface object to the net config object.
@@ -234,12 +364,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_ovs_dpdk_port is not implemented.")
 
+    def del_ovs_dpdk_port(self, ovs_dpdk_port):
+        """Delete a OvsDpdkPort object to the net config object.
+
+        :param ovs_dpdk_port: The OvsDpdkPort object to be deleted.
+        """
+        raise NotImplementedError("del_ovs_dpdk_port is not implemented.")
+
     def add_ovs_dpdk_bond(self, ovs_dpdk_bond):
         """Add a OvsDpdkBond object to the net config object.
 
         :param ovs_dpdk_bond: The OvsDpdkBond object to add.
         """
         raise NotImplementedError("add_ovs_dpdk_bond is not implemented.")
+
+    def del_ovs_dpdk_bond(self, ovs_dpdk_bond):
+        """Delete a OvsDpdkBond object to the net config object.
+
+        :param ovs_dpdk_bond: The OvsDpdkBond object to be deleted.
+        """
+        raise NotImplementedError("del_ovs_dpdk_bond is not implemented.")
 
     def add_sriov_pf(self, sriov_pf):
         """Add a SriovPF object to the net config object.
@@ -248,12 +392,26 @@ class NetConfig(object):
         """
         raise NotImplementedError("add_sriov_pf is not implemented.")
 
+    def del_sriov_pf(self, sriov_pf):
+        """Delete a SriovPF object to the net config object.
+
+        :param sriov_pf: The SriovPF object to be deleted.
+        """
+        raise NotImplementedError("del_sriov_pf is not implemented.")
+
     def add_sriov_vf(self, sriov_vf):
         """Add a SriovVF object to the net config object.
 
         :param sriov_vf: The SriovVF object to add.
         """
         raise NotImplementedError("add_sriov_vf is not implemented.")
+
+    def del_sriov_vf(self, sriov_vf):
+        """Delete a SriovVF object to the net config object.
+
+        :param sriov_vf: The SriovVF object to be deleted.
+        """
+        raise NotImplementedError("del_sriov_vf is not implemented.")
 
     def add_vpp_interface(self, vpp_interface):
         """Add a VppInterface object to the net config object.