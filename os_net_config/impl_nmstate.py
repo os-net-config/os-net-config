b57f6f1bae65624c0f8d38e8ffc7abf033ced92f
sbarak <sbarak@redhat.com>
2024-11-20 15:36:14 +0200

    route: adding the option to add route via next-hop-interface
    this PR will allow to use 'self' str in next_hop inorder to route
    though the device.
    {"ip_netmask": "0.0.0.0/0", "next_hop": "self" }  or "routes":
    [{"ip_netmask": "::/0", "next_hop": "self" }]
    
    Signed-off-by: sbarak <sbarak@redhat.com>

--- os_net_config/impl_nmstate.py
+++ os_net_config/impl_nmstate.py
@@ -992,8 +992,11 @@ class NmstateNetConfig(os_net_config.NetConfig):
             if route.ip_netmask:
                 route_data[NMRoute.DESTINATION] = route.ip_netmask
             if route.next_hop:
-                route_data[NMRoute.NEXT_HOP_ADDRESS] = route.next_hop
-                route_data[NMRoute.NEXT_HOP_INTERFACE] = interface_name
+                if route.next_hop in "self":
+                    route_data[NMRoute.NEXT_HOP_INTERFACE] = interface_name
+                else:
+                    route_data[NMRoute.NEXT_HOP_ADDRESS] = route.next_hop
+                    route_data[NMRoute.NEXT_HOP_INTERFACE] = interface_name
                 if route.default:
                     if ":" in route.next_hop:
                         route_data[NMRoute.DESTINATION] = \
