47d844cc85c565995154fb0e7ae453758a745bd2
Karthik Sundaravel <ksundara@redhat.com>
2025-02-15 21:40:01 +0530

    migrate: skip deleting interfaces
    During migration, its not necessary to delete all the interfaces.
    Certain interfaces needs the presence of ifcfg files for it to be
    created during reboot. Since the ifcfg files of all the devices are
    removed, the deletion of those types of devices are not performed.
    
    Signed-off-by: Karthik Sundaravel <ksundara@redhat.com>

--- os_net_config/impl_ifcfg.py
+++ os_net_config/impl_ifcfg.py
@@ -901,8 +901,7 @@ class IfcfgNetConfig(os_net_config.NetConfig):
 
         :param interface: The Interface object to be deleted.
         """
-        logger.info("%s: Deleting", interface)
-        self._del_common(interface)
+        return
 
     def add_vlan(self, vlan):
         """Add a Vlan object to the net config object.
@@ -923,8 +922,7 @@ class IfcfgNetConfig(os_net_config.NetConfig):
 
         :param vlan: The vlan object to be deleted.
         """
-        logger.info("%s: Deleting vlan ", vlan)
-        self._del_common(vlan)
+        return
 
     def add_ivs_interface(self, ivs_interface):
         """Add a ivs_interface object to the net config object.
@@ -1076,8 +1074,7 @@ class IfcfgNetConfig(os_net_config.NetConfig):
 
         :param bond: The LinuxBond object to be deleted.
         """
-        logger.info("%s: Deleting linux bond", bond.name)
-        self._del_common(bond)
+        return
 
     def add_linux_team(self, team):
         """Add a LinuxTeam object to the net config object.
@@ -1140,8 +1137,7 @@ class IfcfgNetConfig(os_net_config.NetConfig):
 
         :param ib_interface: The InfiniBand interface object to be deleted
         """
-        logger.info("%s: Deleting ib_interface", ib_interface.name)
-        self._del_common(ib_interface)
+        return
 
     def add_ib_child_interface(self, ib_child_interface):
         """Add an InfiniBand child interface object to the net config object.
@@ -1167,8 +1163,7 @@ class IfcfgNetConfig(os_net_config.NetConfig):
         :param ib_child_interface: The InfiniBand child
          interface object to be deleted.
         """
-        logger.info("%s: Deleting ib_child_interface", ib_child_interface.name)
-        self._del_common(ib_child_interface)
+        return
 
     def add_ovs_dpdk_port(self, ovs_dpdk_port):
         """Add a OvsDpdkPort object to the net config object.
