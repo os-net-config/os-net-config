54027dd3ad0f2f67d5aac3f087a53d4bd803504a
vcandapp <vcandapp@redhat.com>
2026-02-19 13:38:13 +0530

    ifcfg: update accept_ra when no static+dynamic addr is specified
    This change adds proper IPv6 RA handling for interfaces without addresses set,
    preventing unintended IPv6 autoconfiguration based on global sysctl settings.
    
    - If IPv6 is enabled globally AND RA is disabled by default, update ifcfg-
      to disable auto-conf
    - Added new methods to read sysctl
    - Updated tests cases for few possible scenarios
    
    Closes: https://issues.redhat.com/browse/OSPRH-26347
    Signed-off-by: vcandapp <vcandapp@redhat.com>

--- os_net_config/utils.py
+++ os_net_config/utils.py
@@ -978,6 +978,23 @@ def set_accept_ra_sysctl(iface, noop):
                 f.write("0")
 
 
+def get_sysctl_value(sysctl_path):
+    """Read a sysctl value from /proc/sys filesystem
+
+    :param sysctl_path: Sysctl param path
+                        (e.g. 'net.ipv6.conf.all.disable_ipv6')
+    :returns: The sysctl value as a string, or None if not found
+    """
+    filename = "/proc/sys/{}".format(sysctl_path.replace('.', '/'))
+    try:
+        if os.path.exists(filename):
+            with open(filename, 'r') as f:
+                return f.read().strip()
+    except (IOError, OSError) as e:
+        logger.debug("Failed to read sysctl %s: %s", sysctl_path, e)
+    return None
+
+
 def set_keep_addr_sysctl(iface, noop):
     """Set /proc/sys/net/ipv6/conf/<iface>/keep_addr_on_down """
     filename = "/proc/sys/net/ipv6/conf/{}/keep_addr_on_down".format(iface)
